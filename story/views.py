58edaa0425a10ad6db6e4ac84f4ba34504bf1481
Mohidul Islam <mohidul.nu@gmail.com>
2019-07-22 23:54:54 +0600

    Shown posts in homepage

--- story/views.py
+++ story/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 
 def home(request):
-    return render(request, 'story/home.html')
+    posts = Post.objects.all()
+    return render(request, 'story/home.html', {'posts': posts})
 
 
 def about(request):
