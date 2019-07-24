f50207d12e98e69832e76bf6787450413520a1b1
Mohidul Islam <mohidul.nu@gmail.com>
2019-07-24 23:10:14 +0600

    Added more test for user registration form

--- users/tests.py
+++ users/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.urls import resolve
 from .views import register
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User
 
 
 class RegisterTests(TestCase):
@@ -23,3 +24,55 @@ class RegisterTests(TestCase):
     def test_contains_form(self):
         form = self.response.context.get('form')
         self.assertIsInstance(form, UserRegisterForm)
+
+    def test_form_inputs(self):
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
+
+    def test_form_has_fields(self):
+        form = UserRegisterForm()
+        expected = ['username', 'email', 'password1', 'password2', ]
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
+
+
+class SuccessfulRegistrationTests(TestCase):
+    def setUp(self):
+        url = reverse('register')
+        data = {
+            'username': 'ifat',
+            'password1': 'abcdef123456',
+            'email': 'ifat@yourstory.org',
+            'password2': 'abcdef123456',
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('story-home')
+
+    def test_redirection(self):
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authentication(self):
+        response = self.client.get(self.home_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
+
+
+class InvalidUserRegisterTests(TestCase):
+    def setUp(self):
+        url = reverse('register')
+        self.response = self.client.post(url, {})
+
+    def test_register_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
