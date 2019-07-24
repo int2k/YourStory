4637ff26302c7e972d44c22a5977c086cf9619e2
Mohidul Islam <mohidul.nu@gmail.com>
2019-07-24 22:05:02 +0600

    Added some unit test for register view

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.urls import resolve
+from .views import register
+from .forms import UserRegisterForm
 
-# Create your tests here.
+
+class RegisterTests(TestCase):
+    def setUp(self):
+        url = reverse('register')
+        self.response = self.client.get(url)
+
+    def test_register_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_register_url_resolves_register_view(self):
+        view = resolve('/register/')
+        self.assertEquals(view.func, register)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserRegisterForm)
