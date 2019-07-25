c1aea3dd7523e3f724fcd2c5d5cf07466850933d
Mohidul Islam <mohidul.nu@gmail.com>
2019-07-25 16:01:09 +0600

    Add some unit test for login

--- users/tests.py
+++ users/tests.py
@@ -76,3 +76,25 @@ class InvalidUserRegisterTests(TestCase):
 
     def test_dont_create_user(self):
         self.assertFalse(User.objects.exists())
+
+
+class LoginTests(TestCase):
+    def setUp(self):
+        url = reverse('login')
+        self.response = self.client.get(url)
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+
+    def test_login_status_code(self):
+        self.assertEquals(self.response.status_code, 200)
+
+    def test_form_inputs(self):
+        self.assertContains(self.response, '<input', 3)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="password"', 1)
+
+    def test_login(self):
+        User.objects.create_user(**self.credentials)
+        response = self.client.post('/login/', self.credentials, follow=True)
+        self.assertTrue(response.context['user'].is_active)
