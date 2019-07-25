ef5244248c4d13297d9da8adae81236853dd252e
Mohidul Islam <mohidul.nu@gmail.com>
2019-07-25 16:19:21 +0600

    Added profile view

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import login as auth_login
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -16,3 +17,8 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
