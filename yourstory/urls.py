84676862b489a6dc172354d2ade7e5ffcafa27b0
Mohidul Islam <mohidul.nu@gmail.com>
2019-07-25 20:05:27 +0600

    Add update functionality on app

--- yourstory/urls.py
+++ yourstory/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('', include('story.urls')),
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
+    path('update_profile/', user_views.update_profile, name='update_profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
