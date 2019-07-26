c4df44087993d0762c42300e20f135c9fe4a228c
Mohidul Islam <mohidul.nu@gmail.com>
2019-07-26 23:49:06 +0600

    Added some unit test for new post

--- story/tests.py
+++ story/tests.py
@@ -64,3 +64,60 @@ class AboutPageTests(SimpleTestCase):
     def test_about_page_does_not_contain_incorrect_html(self):
         response = self.client.get('/about/')
         self.assertNotContains(response, 'Hi there! I should not be on the page.')
+
+
+class NewPostTests_Before_Authentication(TestCase):
+    def test_create_post_page_status_code(self):
+        response = self.client.get('/post/new/')
+        self.assertEquals(response.status_code, 302)
+
+    def test_create_post_view_url_by_name(self):
+        response = self.client.get(reverse('post-create'))
+        self.assertEquals(response.status_code, 302)
+
+
+class NewPostTests_After_Authentication(TestCase):
+
+    def setUp(self):
+        User.objects.create_user(username='mohit', email='mohit@sharestory.com', password='123')
+        self.client.login(username='mohit', password='123')
+
+    def test_create_post_page_status_code(self):
+        response = self.client.get('/post/new/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_create_post_view_url_by_name(self):
+        response = self.client.get(reverse('post-create'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_create_post_view_uses_correct_template(self):
+        response = self.client.get(reverse('post-create'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'story/post_form.html')
+
+    def test_csrf(self):
+        response = self.client.get(reverse('post-create'))
+        self.assertContains(response, 'csrfmiddlewaretoken')
+
+    def test_create_post_with_valid_post_data(self):
+        data = {
+            'title': 'Test title',
+            'content': 'Test content'
+        }
+        self.client.post(reverse('post-create'), data)
+        self.assertTrue(Post.objects.exists())
+
+    def test_create_post_with_invalid_post_data(self):
+        response = self.client.post(reverse('post-create'), {})
+        form = response.context.get('form')
+        self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
+
+    def test_create_post_with_empty_post_data(self):
+        data = {
+            'title': '',
+            'content': ''
+        }
+        response = self.client.post(reverse('post-create'), data)
+        self.assertEquals(response.status_code, 200)
+        self.assertFalse(Post.objects.exists())
