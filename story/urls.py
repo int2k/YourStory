c95fd8dcdbd9212ba4b0a35bb93ed2ceffbae3d4
Mohidul Islam <mohidul.nu@gmail.com>
2019-07-26 17:59:43 +0600

    Work with post update

--- story/urls.py
+++ story/urls.py
@@ -3,12 +3,14 @@ from .views import (
     PostListView,
     AboutPageView,
     PostCreateView,
-    PostDetailView
+    PostDetailView,
+    PostUpdateView
 )
 
 urlpatterns = [
     path('', PostListView.as_view(), name='story-home'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
     path('about/', AboutPageView.as_view(), name='story-about'),
 ]
--- story/views.py
+++ story/views.py
@@ -1,10 +1,11 @@
 from .models import Post
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     TemplateView,
     CreateView,
-    DetailView
+    DetailView,
+    UpdateView
 )
 
 
@@ -31,3 +32,18 @@ class PostCreateView(LoginRequiredMixin, CreateView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
